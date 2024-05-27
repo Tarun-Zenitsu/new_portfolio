243da82bf9522a2b50877b07d35b299421656956
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-05-27 18:30:55 +0530

    Project copmpleted

---
+++ components/Footer.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <footer id="contact" className="pt-20 pb-10 w-full">
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take <span className="text-purple">your</span> digital
+          presence to next level?
+        </h1>
+        <p className="text-white-200 md:my-10 my-5 text-center">
+          Reach out to me today and let's discuss how I can help you to achiev
+          your goals.
+        </p>
+        <a href="mailto:tarunkumbhar1998@gmail.com">
+          <MagicButton
+            title="let's get in tuch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Tarun
+        </p>
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((Profile) => (
+            <div
+              key={Profile.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-150 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={Profile.img} alt={Profile.img} width={20} height={20} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -42,9 +42,13 @@ const RecentProjects = () => {
                   ))}
                 </div>
                 <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                  <a
+                    className="flex lg:text-xl md:text-xs text-sm text-purple"
+                    href={link}
+                    target="_blank"
+                  >
                     Check Live Site
-                  </p>
+                  </a>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
